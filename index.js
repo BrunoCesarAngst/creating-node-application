5f07f2d9b3637c5deb1866277ef7049448abadcc
brunocesarangst <bruno.angst@rede.ulbra.br>
2019-12-02 20:16:36 -0300

    criado o CRUD

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ const express = require('express');
 // 2º Criando um servidor.
 const server = express();
 
+// 12º Avisando o express para ler json do corpo da requesição
+// o use faz um ligação entre a estancia server com o modulo express.json().
+server.use(express.json());
+
 /**
  * 4º Criamos uma primeira rota.
  * Criamos o método get por ser possível visualizar no navegador, pois, esse é o
@@ -20,7 +24,7 @@ const server = express();
  
   // return res.json({ message: 'Hello World!' })
   // res.json para retornar uma estrutura de dados em um json.
-// })
+// });
 
 /**
  * 6º - Route params
@@ -35,17 +39,72 @@ const server = express();
  
 //   return res.json({ message: `Hello ${name}!` })
 //   // usando template strings.
-// })
+// });
 
 // 8º consumindo Route params
-server.get('/users/:id', (req, res) => {
-  // const id = req.params.id;
-    // dá para simplificar com desestruturação do ECMA6 assim:
-  const { id } = req.params;
+// server.get('/users/:id', (req, res) => {
+//   // const id = req.params.id;
+//     // dá para simplificar com desestruturação do ECMA6 assim:
+//   const { id } = req.params;
  
-  return res.json({ message: `Buscando o usuário ${id}!` })
-  // usando template strings.
-})
+//   return res.json({ message: `Buscando o usuário ${id}!` })
+//   // usando template strings.
+// });
+
+// 9º Criando um tipo de armazenamento e esse, uma variável no mesmo arquivo
+// const users = ['Breno', 'Micaela', 'Margarete', 'Bruno'];
+
+// server.get('/users/:index', (req, res) => {
+//   // dá para simplificar com desestruturação do ECMA6 assim:
+//   const { index } = req.params;
+ 
+//   return res.json(users[index]);
+// });
+
+// 10º CRUD - Create, Read, Update, Delete
+
+const users = ['Breno', 'Micaela', 'Margarete', 'Bruno'];
+
+// mostra todos os usuários
+server.get('/users', (req, res) => {
+  return res.json(users);
+});
+
+// mostra um usuário
+server.get('/users/:index', (req, res) => {
+  // dá para simplificar com desestruturação do ECMA6 assim:
+  const { index } = req.params;
+ 
+  return res.json(users[index]);
+});
+
+// 11º cria um novo usuário
+server.post('/users', (req, res) => {
+  const { name } = req.body;
+
+  users.push(name);
+
+  return res.json(users);
+});
+
+// 13º alterando um usuário
+server.put('/users/:index', (req, res) => {
+  const { index } = req.params;
+  const { name } = req.body;
+
+  users[index] = name;
+
+  return res.json(users);
+});
+
+// 14º deletando um usuário
+server.delete('/users/:index', (req, res) => {
+  const { index } = req.params;
+
+  users.splice(index, 1);
+
+  return res.send();
+});
 
 // 3º Definindo uma porta que ouve o servidor node dessa aplicação.
 server.listen(3000);
