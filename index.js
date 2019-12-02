8a743074602d9164fb00f29a39ac0a325b7c2fd3
brunocesarangst <bruno.angst@rede.ulbra.br>
2019-12-02 14:12:58 -0300

    Query & Route params

--- index.js
+++ index.js
@@ -9,15 +9,42 @@ const server = express();
  * Criamos o método get por ser possível visualizar no navegador, pois, esse é o
  * método que o navegador usa para mostrar as informações de um endereço ou um
  * site.
-*/ 
-server.get('/teste', (req, res) => {
+*/
+
+// server.get('/users', (req, res) => {
   /**
    * 5º
    * req são todos os dados da nossa requisição.
    * res são as repostas para o front end.
   */
-  return res.json({ message: 'Hello World!' })
+ 
+  // return res.json({ message: 'Hello World!' })
   // res.json para retornar uma estrutura de dados em um json.
+// })
+
+/**
+ * 6º - Route params
+ * Query params = ?users=1
+ * Route params = /users/1
+ * Request body = { "name": "Fulbar", "email": "fulbar@fulbar.com" }
+ */
+
+// 7º consumindo Query params
+// server.get('/users', (req, res) => {
+//   const name = req.query.name;
+ 
+//   return res.json({ message: `Hello ${name}!` })
+//   // usando template strings.
+// })
+
+// 8º consumindo Route params
+server.get('/users/:id', (req, res) => {
+  // const id = req.params.id;
+    // dá para simplificar com desestruturação do ECMA6 assim:
+  const { id } = req.params;
+ 
+  return res.json({ message: `Buscando o usuário ${id}!` })
+  // usando template strings.
 })
 
 // 3º Definindo uma porta que ouve o servidor node dessa aplicação.
